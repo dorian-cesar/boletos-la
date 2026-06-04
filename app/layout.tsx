7c855bca1efe90d48dab8569eb709ed11a3bb4ba
Luis O <30819865+hlcxpl@users.noreply.github.com>
2026-06-04 10:15:07 -0400

    Evitar advertencia de hidratacion causada por extensiones

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,8 +35,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="es">
-      <body className={`${inter.variable} antialiased`}>
+    <html lang="es" suppressHydrationWarning>
+      <body className={`${inter.variable} antialiased`} suppressHydrationWarning>
         <GoogleTagManager />
         {children}
       </body>
