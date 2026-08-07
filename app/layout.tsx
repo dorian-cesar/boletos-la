d3e58dbe327ac3060123c935bcba95daf1aff9cf
dorian-cesar <dorian.celu@gmail.com>
2026-08-07 09:40:31 -0400

    add pixel cod

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import GoogleTagManager from "@/components/google-tag-manager";
+import MetaPixel from "@/components/meta-pixel";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -38,6 +39,7 @@ export default function RootLayout({
     <html lang="es" className="scroll-smooth" suppressHydrationWarning>
       <body className={`${inter.variable} antialiased`} suppressHydrationWarning>
         <GoogleTagManager />
+        <MetaPixel />
         <div className="zoom-wrapper">
           {children}
         </div>
